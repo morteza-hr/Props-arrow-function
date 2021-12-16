4748e2f3b54dc48125b6d60bc9fcc55c02469609
Morteza Hashemi <hashemi@ertebat.internet>
2021-12-16 15:24:48 +0330

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Hello from "./component/props";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Hello name = "Hi"/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/props.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Hello = (props) => <h1>HelloWorld! {props.name}</h1>;
+
+export default Hello;
